f1f3c25c1aa54e72be1b3021c4314167f8bb5890
AdarshaSomanna <adarsha.lunia@gmail.com>
2023-04-08 10:01:27 +0530

    feature: completed login

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const eventRoutes = require('./routes/events');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   next();
 });
 
+app.use(authRoutes);
 app.use('/events', eventRoutes);
 
 app.use((error, req, res, next) => {
